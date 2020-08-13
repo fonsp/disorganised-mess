7a3874111eade71dba074f30d809a20c2d92cb66
fonsp <fonsvdplas@gmail.com>
2020-08-13 11:03:08 +0000

    asdf

--- bigbond.jl
+++ bigbond.jl
@@ -1,14 +1,33 @@
 ### A Pluto.jl notebook ###
-# v0.7.2
+# v0.11.5
 
 using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     quote
         local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.peek, el) ? Base.peek(el) : missing
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
         el
     end
 end
+
+# ╔═╡ 66181f40-dd53-11ea-22ad-a7f633462733
+
+
+# ╔═╡ 27d5bed6-7fe8-11ea-1a8a-af798742d71d
+view([1,2,3], 2:end) == [2,3]
+
+# ╔═╡ 61fb01fc-7fe8-11ea-04f6-c1a6fb545a0d
+
+
+# ╔═╡ cccd3524-7fd7-11ea-241b-a35f2ca3e245
+using JSON
+
+# ╔═╡ 1836b868-dd53-11ea-3711-cd4504b2aca0
+tic = Ref(time())
+
 # ╔═╡ e0ce06f8-7fd6-11ea-2b59-ff9d69c47d42
 @bind x html"""
 <canvas id="drawboard" width="200" height="200"></canvas>
@@ -55,6 +74,15 @@ onmove({layerX: 130, layerY: 160})
 </script>
 """
 
+# ╔═╡ 25b8e04a-dd53-11ea-2067-adfc77c6b0fc
+let
+	x
+	toc = time()
+	Δt = toc - tic[]
+	tic[] = toc
+	Δt
+end
+
 # ╔═╡ efb50b94-7fd6-11ea-1d80-f7ddee184780
 (x |> length, rand())
 
@@ -72,12 +100,6 @@ end
 # ╔═╡ cd83f1a0-7fe7-11ea-1546-910dec41906e
 endswith([1,2,3], [2,3, 4, 5])
 
-# ╔═╡ 27d5bed6-7fe8-11ea-1a8a-af798742d71d
-view([1,2,3], 2:end) == [2,3]
-
-# ╔═╡ 61fb01fc-7fe8-11ea-04f6-c1a6fb545a0d
-
-
 # ╔═╡ fed7690c-7fe8-11ea-221c-edefefcd4aef
 begin
 	str = "asdfasdfdfas"
@@ -90,13 +112,13 @@ codeunits(str)|> Vector{UInt8}
 # ╔═╡ ff660100-7fd7-11ea-080f-d762a0764752
 obj = Dict(:a => Dict(:b => str), :c => :d)
 
-# ╔═╡ cccd3524-7fd7-11ea-241b-a35f2ca3e245
-using JSON
-
 # ╔═╡ deb4cf4a-7fd7-11ea-2a63-7b27570cd414
 JSON.Parser.parse( codeunits(JSON.json(obj)) |> Vector{UInt8}) |> length
 
 # ╔═╡ Cell order:
+# ╠═1836b868-dd53-11ea-3711-cd4504b2aca0
+# ╠═66181f40-dd53-11ea-22ad-a7f633462733
+# ╠═25b8e04a-dd53-11ea-2067-adfc77c6b0fc
 # ╠═efb50b94-7fd6-11ea-1d80-f7ddee184780
 # ╠═e0ce06f8-7fd6-11ea-2b59-ff9d69c47d42
 # ╠═a8eb7240-7fe8-11ea-3379-e1f3cd6f4684
