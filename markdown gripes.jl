04027158de69d58d89b7ed3ac397edb9e5f9aa1f
Fons van der Plas <fonsvdplas@gmail.com>
2020-09-02 15:41:42 +0200

    afsd

--- markdown gripes.jl
+++ markdown gripes.jl
@@ -40,17 +40,30 @@ md"""
 	asdf
 """
 
+# ╔═╡ 465e2a6c-ecaf-11ea-3524-d52dd763c2db
+begin
+	import Example
+	sqrt(1)
+	for x in z 
+		y
+	end
+	md"""asdf
+	asdf
+	"""
+	r(z())
+end
+
+# ╔═╡ 42e084a2-ecac-11ea-25d6-af4badc26723
+sleep(1)
+
 # ╔═╡ 6892f8fa-ec92-11ea-31b3-791acc4094e4
 md"### 4 spaces"
 
 # ╔═╡ 3a4ba37a-ec92-11ea-18a8-f5730032dfa7
 md"""
 [^this]:
-    asdf
+asdf
 """
-
-# ╔═╡ 24e545ec-ec95-11ea-2be5-8fdf1caee5ba
-
 
 # ╔═╡ 23bc4512-ec95-11ea-0136-7f36c73fcd57
 import Pkg
@@ -116,19 +129,27 @@ math!
 huh
 """
 
+# ╔═╡ 0c17cf0a-ed04-11ea-1aa4-1d5567677448
+md"""
+a $$a$$
+
+a
+"""
+
 # ╔═╡ Cell order:
 # ╟─a6dcd8ec-ec92-11ea-16ff-ed6cdef0e01b
 # ╟─257c49f4-ec92-11ea-0945-b5c28f7746d6
-# ╠═22187472-ec92-11ea-29eb-33e609eba048
+# ╟─22187472-ec92-11ea-29eb-33e609eba048
 # ╟─2bedf332-ec92-11ea-1806-33b160b0d70f
-# ╠═19f10ac0-ec92-11ea-348f-57a0ebcb9aa5
+# ╟─19f10ac0-ec92-11ea-348f-57a0ebcb9aa5
 # ╟─b2f99764-ec92-11ea-2767-895d1a0ffeb5
 # ╟─64387e44-ec92-11ea-052e-45da3187cd30
 # ╟─494c58c4-ec92-11ea-0e93-536e9a1d6537
 # ╠═3635e2fc-ec92-11ea-1d3d-b9778b25a6f1
+# ╠═465e2a6c-ecaf-11ea-3524-d52dd763c2db
+# ╠═42e084a2-ecac-11ea-25d6-af4badc26723
 # ╟─6892f8fa-ec92-11ea-31b3-791acc4094e4
 # ╠═3a4ba37a-ec92-11ea-18a8-f5730032dfa7
-# ╠═24e545ec-ec95-11ea-2be5-8fdf1caee5ba
 # ╠═23e47280-ec95-11ea-207d-5523d4d1becb
 # ╠═23bc4512-ec95-11ea-0136-7f36c73fcd57
 # ╠═a6f66554-ec91-11ea-3115-7d4e3c3393e4
@@ -142,3 +163,4 @@ huh
 # ╠═20f13d40-ec9c-11ea-3e3a-4f7cdf11b68c
 # ╠═b5c49d74-ec90-11ea-3802-8d86913d0d3d
 # ╠═816bbe72-ec9a-11ea-3b5b-bde7e2a2ec9e
+# ╠═0c17cf0a-ed04-11ea-1aa4-1d5567677448
