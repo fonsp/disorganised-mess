84cf401cd489e4eda5e7ad4ec94a95d2f796beb5
fonsp <fonsvdplas@gmail.com>
2020-10-12 16:11:37 +0000

    asd

--- margo forward.jl
+++ margo forward.jl
@@ -28,6 +28,9 @@ using Plots
 # ╔═╡ 81a40932-0c9e-11eb-11fb-bd426c8b8f5c
 using PlutoUI
 
+# ╔═╡ 44b9adaa-0ca1-11eb-3f8a-3f0c89b8a434
+Dict(1 => 2, Dict(3=>4)...)
+
 # ╔═╡ 7f2e0f12-0c9c-11eb-165e-2dab4b00162e
 dt = 10
 
@@ -99,6 +102,9 @@ end
 # ╔═╡ 8eb8885e-0c9c-11eb-32d5-73435fecad9d
 model.controls
 
+# ╔═╡ 28d8c1e2-0ca2-11eb-0422-c752a17b073d
+ClimateMARGO.IO.JSON2.write(model.controls.mitigate)
+
 # ╔═╡ a776f150-0c9c-11eb-3d4f-01a7f9af049d
 let
 	M = plot(t, model.controls.mitigate, ylim=(0,1))
@@ -113,6 +119,7 @@ end
 # ╠═cf4a9334-0c9b-11eb-29e7-55615b7e91be
 # ╠═2c9f3654-0c9c-11eb-0c04-25f8b21c31a3
 # ╠═81a40932-0c9e-11eb-11fb-bd426c8b8f5c
+# ╠═44b9adaa-0ca1-11eb-3f8a-3f0c89b8a434
 # ╠═7f2e0f12-0c9c-11eb-165e-2dab4b00162e
 # ╠═10b2d4de-0c9d-11eb-3b01-755a22baf34d
 # ╠═1d37e7ee-0c9d-11eb-3539-a147e3280096
@@ -130,5 +137,6 @@ end
 # ╠═7a18fb72-0c9e-11eb-3b2f-83c084d6d69d
 # ╠═a219464a-0c9e-11eb-2340-23e32a781374
 # ╠═8f3adf8a-0c9e-11eb-0bb3-0393c8f07bc1
+# ╠═28d8c1e2-0ca2-11eb-0422-c752a17b073d
 # ╠═a776f150-0c9c-11eb-3d4f-01a7f9af049d
 # ╠═e9fbf4da-0c9c-11eb-2af9-dbf299ae96e6
