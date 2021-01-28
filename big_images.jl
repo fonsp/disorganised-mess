90ef727cab87195c2ce31f5eaeb8c009556cc291
Fons van der Plas <fonsvdplas@gmail.com>
2021-01-29 00:11:46 +0100

    Update big_images.jl

--- big_images.jl
+++ big_images.jl
@@ -13,8 +13,15 @@ macro bind(def, element)
     end
 end
 
+# ╔═╡ a3a04d5f-b0f0-4740-9b37-92570864f142
+high_res = true
+
 # ╔═╡ a822ac82-5691-4f8e-a60a-1a4582cf59e7
-dog_file = download("https://upload.wikimedia.org/wikipedia/commons/e/ef/Pluto_in_True_Color_-_High-Res.jpg")
+dog_file = if high_res
+		download("https://upload.wikimedia.org/wikipedia/commons/e/ef/Pluto_in_True_Color_-_High-Res.jpg")
+else
+	download("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/240px-Pluto_in_True_Color_-_High-Res.jpg")
+end
 
 # ╔═╡ 627461ce-9e80-4707-b0ba-ddc6bb9b4269
 begin
@@ -25,10 +32,16 @@ begin
 end
 
 # ╔═╡ 5ce8ebc6-b509-42f0-acd5-8008673b04ab
-md" $(filesize(dog_file) / 1000) kB"
+md"Downloaded image is $(filesize(dog_file) / 1000) kB"
 
 # ╔═╡ 03307e43-cb61-4321-95ac-7bbb16e0cfc6
 @bind x html"<input type=range max=10000>"
+
+# ╔═╡ 8559d502-847e-4506-b9d6-031be014d262
+import Distributed
+
+# ╔═╡ bfe37b62-576e-46e7-bf28-a7ac2483ffd8
+# Distributed.remotecall_eval(Main, [1], :(GC.gc()))
 
 # ╔═╡ b18c2329-18d7-4041-962c-0ef98f8aa591
 x
@@ -39,15 +52,24 @@ x
 # ╔═╡ 5539db10-b0d2-48b6-8985-ef437b8ae0b5
 show_dogs = true
 
+# ╔═╡ 1f48fe19-3ee8-44ac-a591-7b4df2d2f93a
+md"""
+This cell will have very large Uint8Arrays in the output body
+"""
+
 # ╔═╡ 8b0de539-42ef-4ce0-920d-af074f6dba1f
 show_dogs && fill(Dog(), 20)
 
 # ╔═╡ Cell order:
 # ╠═627461ce-9e80-4707-b0ba-ddc6bb9b4269
+# ╠═a3a04d5f-b0f0-4740-9b37-92570864f142
 # ╠═a822ac82-5691-4f8e-a60a-1a4582cf59e7
-# ╠═5ce8ebc6-b509-42f0-acd5-8008673b04ab
+# ╟─5ce8ebc6-b509-42f0-acd5-8008673b04ab
 # ╠═03307e43-cb61-4321-95ac-7bbb16e0cfc6
+# ╠═8559d502-847e-4506-b9d6-031be014d262
+# ╠═bfe37b62-576e-46e7-bf28-a7ac2483ffd8
 # ╠═b18c2329-18d7-4041-962c-0ef98f8aa591
 # ╠═87620bae-16b3-48fe-a10d-5b906c7b337f
 # ╠═5539db10-b0d2-48b6-8985-ef437b8ae0b5
+# ╟─1f48fe19-3ee8-44ac-a591-7b4df2d2f93a
 # ╠═8b0de539-42ef-4ce0-920d-af074f6dba1f
