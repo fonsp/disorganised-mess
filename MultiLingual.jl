97a020a0111b42b3209d11956801003a328bef18
fonsp <fonsvdplas@gmail.com>
2021-10-03 12:17:03 +0000

    Deploying to gh-pages from  @ 72444974cdea5f2e78cbcfc026521d4c8aeb8da0 🚀

--- MultiLingual.jl
+++ MultiLingual.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.16.0
+# v0.16.1
 
 using Markdown
 using InteractiveUtils
@@ -9,6 +9,70 @@ using HypertextLiteral
 
 # ╔═╡ 1378705d-a3c6-4971-9ac8-cd2a12cf2ef9
 begin
+	"""
+	A way to provide multiple translations of the same object. When someone else views your notebook, _their_ system/browser preferences are used to show the translation in _their preferred language_. This allows you to write multilingual material!
+
+	# Example
+	```julia
+	MultiLingual([
+		"fr" => "Salut tous le monde !",
+		"en" => "Hello world!", 
+	])
+	```
+
+	When the result is viewed on a computer with English set as the prefered language, it will say _Hello world!_, and _Salut tous le monde !_ on a French computer.
+
+	# Not just strings
+
+	`MultiLingual` can be used to provide translations for any object. Most commonly, it can be used to switch between markdown proses:
+	```julia
+	MultiLingual([
+		"fr" => md"\""
+		# Salut tous le monde !
+	
+		Ma planète préferée est **Pluto**.
+		"\"",
+	
+		"en" => md"\""
+		# Hello world!
+	
+		My favourite planet is **Pluto**.
+		"\"", 
+	])
+	```
+
+	You can even use `MultiLingual` to show a different image or plot depending on the preferred language!
+	
+	# Fallback language
+
+	When none of the provided translations is a preferred language of the reader, the **first** version will be shown. In the example above, this would be the `"fr"` version.
+
+	# Language codes and dialects
+	As the language tag, we use a 2-character [ISO 639-1 code](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes), like `"fr"` or `"en"`. Using the full name of the language (e.g. `"Français"`) will not work, but you can use [this table to find the 2-character code for many languages](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes). 
+
+	## Regional variants, dialects
+	
+	If you want to provide translations for regional versions of the same language, you can use a [BCP47 codes](http://tools.ietf.org/html/bcp47) as the language tag, containing a **2-character language tag** and an optional **2-character region tag**. Examples are: `"en"` (English), `"en-US"` (_American_ English), `"de"` (German) and `"de-AT"` (_Austrian_ German).
+
+	If you are providing translations for **multiple regional variants of a language**, use the full BCP47 code, including the region:
+	```julia
+	MultiLingual([
+		"en-GB" => "Those colours are delighful!", 
+		"en-US" => "Sick colors yo!", 
+		"nl" => "Leuke kleurtjes!",
+	])
+	```
+
+	However, if you are only providing **one translation per language** (intended for speakers of any region within a language), then **we recommend not specifying the region**.
+	```julia
+	MultiLingual([
+		"en" => "Those colours are nice!", 
+		"nl" => "Leuke kleurtjes!",
+	])
+	```
+
+	
+	"""
 	struct MultiLingual
 		contents::Vector{Pair{String,Any}}
 	end
@@ -19,9 +83,38 @@ begin
 				<script>
 				const wrapper = currentScript.parentElement
 				const spans = Array.from(wrapper.childNodes).filter(n => n.tagName === "SPAN")
-				
-				console.log(spans)
-				
+				const lang = (s) => s.getAttribute("lang")
+				// const defined_langs = spans.map(lang)
+
+				for (const l of navigator.languages) {
+					for (const s of spans) {
+						if(
+							lang(s).toLowerCase() === l.toLowerCase() || 
+							lang(s).toLowerCase() === l.toLowerCase().split("-")[0]
+						) {
+					
+							s.style.display = null
+							return
+						}
+					}
+				}
+				for (const l of navigator.languages) {
+					for (const s of spans) {
+						if(
+							lang(s).toLowerCase().split("-")[0] === l.toLowerCase().split("-")[0]
+						) {
+					
+							s.style.display = null
+							return
+						}
+					}
+				}
+
+				// if no match was found, display the first span
+				if(spans.length > 0){
+					spans[0].style.display = null
+				}
+		
 				</script>
 				
 				<style>
@@ -30,10 +123,10 @@ begin
 				</style>
 				
 				$((
-					@htl("
-					<span lang=$(lang)>$(el)</span>
-					")
-					for (lang, el) in ml.contents
+					@htl("""
+					<span lang=$(lang) style=$(false ? "" : "display: none")>$(el)</span>
+					""")
+					for (i,(lang, el)) in enumerate(ml.contents)
 				))
 				</span>
 				
@@ -41,11 +134,52 @@ begin
 	end
 end
 
+# ╔═╡ 00bf8e98-b6e9-425d-a76a-7cf3608b6a71
+MultiLingual([
+	"en" => md"""
+	# This is some English text!
+
+	Hellozers!
+	""", 
+	
+	"fr" => md"""
+	# Voici un texte en Français!
+	
+	Cici n'est pas un texte
+	"""
+])
+
 # ╔═╡ 33fb35e6-f935-4007-a53d-b66df49866ac
-ml = MultiLingual(["en" => "English!", "fr" => "frnenchh"])
+ml = MultiLingual(["en" => "This is some English text!", "fr" => "Salut tous le monde !"])
 
 # ╔═╡ 7c7eb3ff-baf6-4a96-9337-47fda60d4ca2
-ml2 = MultiLingual(["en" => "English!", "fr" => ml])
+ml2 = MultiLingual(["asdfasdf" => "show me", "kjewfkjh" => "not me"])
+
+# ╔═╡ e3992501-6f29-43ff-a0a6-7c9080c0bf9e
+MultiLingual(["asdf" => "fake language", "en-GB" => "this is englishz", "kjewfkjh" => "not me"])
+
+# ╔═╡ 23530429-93ab-41c4-aafe-8071614ba19f
+md"""
+# Hello $(MultiLingual(["en" => "English", "fr" => "French",])) speaker
+"""
+
+# ╔═╡ 347220f8-e29c-4944-a2b9-964c9479a389
+
+
+# ╔═╡ 35bf4d11-2c31-4a2e-8f20-3a4dbc7267b9
+md"""
+TODO
+"""
+
+# ╔═╡ f15f351f-bae4-458c-abec-6bb6f475d48a
+@htl("""
+<div style='font-family: system-ui'><span style='font-size: .9em'>文</span>A</div>
+""")
+
+# ╔═╡ 993f1096-d9b1-468b-a739-5906d3ad932f
+@htl("""
+<img style='height: 1em; width: 1em;' src='https://cdn.jsdelivr.net/gh/ionic-team/ionicons@5.5.2/src/svg/language-outline.svg'>
+""")
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -67,9 +201,16 @@ version = "0.9.0"
 """
 
 # ╔═╡ Cell order:
+# ╠═00bf8e98-b6e9-425d-a76a-7cf3608b6a71
 # ╠═c35f0e90-1586-11ec-2bb5-abc0ab1a4a27
 # ╠═1378705d-a3c6-4971-9ac8-cd2a12cf2ef9
 # ╠═33fb35e6-f935-4007-a53d-b66df49866ac
 # ╠═7c7eb3ff-baf6-4a96-9337-47fda60d4ca2
+# ╠═e3992501-6f29-43ff-a0a6-7c9080c0bf9e
+# ╠═23530429-93ab-41c4-aafe-8071614ba19f
+# ╠═347220f8-e29c-4944-a2b9-964c9479a389
+# ╠═35bf4d11-2c31-4a2e-8f20-3a4dbc7267b9
+# ╠═f15f351f-bae4-458c-abec-6bb6f475d48a
+# ╠═993f1096-d9b1-468b-a739-5906d3ad932f
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
